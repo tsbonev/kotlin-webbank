4925c640465d2741ed0bb59ce13bcc0b7fed5874
Tsvetozar Bonev <tsbonev@gmail.com>
2018-07-23 17:16:03 +0300

    Add cookie to login

--- src/main/java/com/clouway/bankapp/adapter/web/LoginController.kt
+++ src/main/java/com/clouway/bankapp/adapter/web/LoginController.kt
@@ -1,10 +1,12 @@
 package com.clouway.bankapp.adapter.web
 
 import com.clouway.bankapp.core.User
+import com.clouway.bankapp.core.UserRegistrationRequest
 import com.clouway.bankapp.core.UserRepository
 import spark.Request
 import spark.Response
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import java.util.*
 
 /**
  * @author tsbonev@gmail.com
@@ -13,6 +15,7 @@ class LoginController(private val userRepo: UserRepository,
                       private val transformer: JsonTransformer) {
 
     fun doGet(req: Request, res: Response): User{
+
         res.type("application/json")
         val user = userRepo.getByUsername(req.params("username"))
 
@@ -21,6 +24,20 @@ class LoginController(private val userRepo: UserRepository,
         }
         else{
             throw NotImplementedException()
+        }
+    }
+
+    fun doPost(req: Request, res: Response) {
+
+        val request = transformer.from(req.body(), UserRegistrationRequest::class.java)
+
+        val actualUser = userRepo.getByUsername(request.username)
+        return if(actualUser.isPresent && actualUser.get().password == request.password){
+
+            return res.status(200)
+
+        }else{
+            res.status(400)
         }
     }
 
--- src/main/java/server/AppBootstrap.kt
+++ src/main/java/server/AppBootstrap.kt
@@ -7,10 +7,27 @@ import com.clouway.bankapp.adapter.web.LoginController
 import com.clouway.bankapp.adapter.web.RegisterController
 import com.clouway.bankapp.adapter.web.TransactionController
 import spark.Filter
+import spark.Response
 import spark.Spark.*
 import spark.kotlin.before
 import spark.servlet.SparkApplication
-import java.awt.SystemColor.window
+import java.util.*
+
+
+/**
+ * Adds a cookie to the user's browser
+ * and returns its value.
+ *
+ * @param res response
+ * @return created cookie's value
+ */
+fun addCookie(res: Response): String {
+
+    val UUIDValue = UUID.randomUUID().toString()
+    res.cookie("/", "SID", UUIDValue, 60000, false, true)
+    return UUIDValue
+
+}
 
 class AppBootstrap : SparkApplication{
     override fun init() {
@@ -47,6 +64,7 @@ class AppBootstrap : SparkApplication{
         before(Filter { req, res -> res.header("Access-Control-Allow-Origin", "*") })
 
         before(Filter { req, res ->
+            addCookie(res)
             res.raw().characterEncoding = "UTF-8"
         })
 
@@ -70,6 +88,11 @@ class AppBootstrap : SparkApplication{
         post("/register", "application/json"){
             req, res ->
             registerController.doPost(req, res)
+        }
+
+        post("/login", "application/json"){
+            req, res ->
+            loginController.doPost(req, res)
         }
 
     }
